f799138b1be94f0173fe0f31915defa4f2c37630
Puneeth Annam <puneethannam08@gmail.com>
2020-03-02 12:17:54 -0600

    commit

--- src/main.js
+++ src/main.js
@@ -1,57 +1,17 @@
-const avg = { return Task/Total CountOfStories }
+
 // I just forgot my quiz level 1 question and did quiz level 2 question submitted in level 1. Sorry for that.
         //input 1 is number i.e,CountOfStories and input 2 number i.e, avgTasksPerStory
 
 
-
-const validate = async (event) => {
-  console.log(`triggered validate on ${event.target.id}`)
-  const isValid = event.target.checkValidity();
-  if (isValid) {
-    event.target.nextElementSibling.innerHTML = ''
-  }
-  else {
-    event.target.nextElementSibling.innerHTML = 'Invalid input'
-    event.target.focus()
-  }
-}
-
-const updateWithAdd = async (event) => {
-  document.querySelector('#result').innerHTML = '';
-  if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
-    const regex = /[^a-zA-Z_]/g
-    const s = document.querySelector('#guest').value.replace(regex, '')
-    const i = parseInt(document.querySelector('#firstNumber').value)
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = `${s}, your sum is ${add(i, j)}.`
-    document.querySelector('#result').innerHTML = ans
-  }
-}
-
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.';
-  document.querySelector('#result').innerHTML = joke
-}
-
-// delegate to dynamic elements (e.g. when testing)
-// focusout is like blur, but it bubbles up
-
-document.addEventListener('focusout', event => {
-  if (event.target && event.target.id === 'firstNumber' ||
-    event.target && event.target.id === 'secondNumber') {
-    validate(event)
-  }
-});
-
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'calculateButton') { updateWithAdd(event) }
-});
-
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
-});
-
+        const count = document.querySelector("#CountOfStories");
+        const avg = document.querySelector("#avgTasksPerStory");
+       
+        button.addEventListener("click", calc);
+        
+        function calc(){
+            
+            calcavgTasksPerStory = (TaskPerStory/TotalCountOfStories);
+           
+        }
+        console.log(document.getElementById());
+        
